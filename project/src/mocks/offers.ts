ebcbfd594183b941399d312c004d3f17551d671c
Эрнест <101067653+Nexoner@users.noreply.github.com>
2023-03-27 16:13:10 +0300

    1

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,30 @@
+export const cards = [
+  {
+    id: 0,
+    type: 'Premium',
+    price: '120',
+    description: 'Beautiful &amp; luxurious apartment at great location',
+    img: 'img/apartment-01.jpg',
+  },
+  {
+    id: 1,
+    type: '',
+    price: '132',
+    description: 'Canal View Prinsengracht',
+    img: 'img/apartment-02.jpg',
+  },
+  {
+    id: 2,
+    type: '',
+    price: '180',
+    description: 'Nice, cozy, warm big bed apartment',
+    img: 'img/apartment-03.jpg',
+  },
+  {
+    id: 4,
+    type: '',
+    price: '80',
+    description: 'Wood and stone place',
+    img: 'img/room.jpg',
+  },
+];
--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,41 +1,12 @@
 import Cards from '../../components/app/cards';
 import {Link} from 'react-router-dom';
+import { cards } from '../../mocks/offers';
 
 type CardCount = {
   cardsCount: number;
 };
 
 function MainScreen({ cardsCount }: CardCount): JSX.Element {
-  const cards = [
-    {
-      id: 0,
-      type: 'Premium',
-      price: '120',
-      description: 'Beautiful &amp; luxurious apartment at great location',
-      img: 'img/apartment-01.jpg',
-    },
-    {
-      id: 1,
-      type: '',
-      price: '132',
-      description: 'Canal View Prinsengracht',
-      img: 'img/apartment-02.jpg',
-    },
-    {
-      id: 2,
-      type: '',
-      price: '180',
-      description: 'Nice, cozy, warm big bed apartment',
-      img: 'img/apartment-03.jpg',
-    },
-    {
-      id: 4,
-      type: '',
-      price: '80',
-      description: 'Wood and stone place',
-      img: 'img/room.jpg',
-    },
-  ];
   return (
     <div className="page page--gray page--main">
       <header className="header">
